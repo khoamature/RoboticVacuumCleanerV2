c852e15849955127f5afd21aa66e588a6910175c
himkhoa <nguyendangkhoaa.1922@gmail.com>
2025-06-29 16:12:52 +0700

    V2 Vacuum

---
+++ Assets/Scripts/RobotController2D.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class RobotController2D : MonoBehaviour
+{
+    public float speed = 5f;
+    public bool canMoveDiagonally = true;
+
+    private Rigidbody2D rb;
+    private Vector2 movementInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    void Update()
+    {
+        // Lấy input từ người chơi
+        float moveX = Input.GetAxisRaw("Horizontal"); // A/D hoặc mũi tên trái/phải
+        float moveY = Input.GetAxisRaw("Vertical");   // W/S hoặc mũi tên lên/xuống
+
+        if (!canMoveDiagonally)
+        {
+            if (Mathf.Abs(moveX) > Mathf.Abs(moveY))
+                moveY = 0;
+            else
+                moveX = 0;
+        }
+
+        movementInput = new Vector2(moveX, moveY).normalized;
+
+        // Xoay robot nếu có hướng
+        if (movementInput != Vector2.zero)
+        {
+            RotateRobot(movementInput.x, movementInput.y);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Di chuyển dựa vào input
+        rb.linearVelocity = movementInput * speed;
+    }
+
+    void RotateRobot(float x, float y)
+    {
+        float angle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
---
+++ Assets/Scripts/Update_Collectible_Count.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using TMPro;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Update_Collectible_Count : MonoBehaviour
+{
+    private TextMeshProUGUI collectibleText;
+    public TextMeshProUGUI winText;
+    public Button replayButton;
+
+    private bool winDisplayed = false;
+
+    void Start()
+    {
+        collectibleText = GetComponent<TextMeshProUGUI>();
+        if (collectibleText == null)
+        {
+            Debug.LogError("UpdateCollectibleCount script requires a TextMeshProUGUI component on the same GameObject.");
+            return;
+        }
+
+        if (winText != null)
+        {
+            winText.gameObject.SetActive(false); 
+        }
+
+        if (replayButton != null)
+        {
+            replayButton.gameObject.SetActive(false); 
+        }
+           
+        if (replayButton != null)
+        {
+            replayButton.onClick.AddListener(RestartGame);
+        }
+
+        UpdateCollectibleDisplay();
+        
+    }
+
+    void Update()
+    {
+        UpdateCollectibleDisplay();
+    }
+    private void UpdateCollectibleDisplay()
+    {
+        int totalCollectibles = 0;
+
+        // Check and count objects of type Collectible
+        Type collectibleType = Type.GetType("Collectible");
+        if (collectibleType != null)
+        {
+            totalCollectibles += UnityEngine.Object.FindObjectsByType(collectibleType, FindObjectsSortMode.None).Length;
+        }
+
+        // Optionally, check and count objects of type Collectible2D as well if needed
+        Type collectible2DType = Type.GetType("Collectible2D");
+        if (collectible2DType != null)
+        {
+            totalCollectibles += UnityEngine.Object.FindObjectsByType(collectible2DType, FindObjectsSortMode.None).Length;
+        }
+
+        // Update the collectible count display
+        collectibleText.text = $"Collectibles Remaining: {totalCollectibles}";
+
+        if (totalCollectibles == 0 && !winDisplayed)
+        {
+            if (winText != null)
+                winText.gameObject.SetActive(true);
+
+            if (replayButton != null)
+                replayButton.gameObject.SetActive(true);
+
+            winDisplayed = true;
+        }
+    }
+    public void RestartGame()
+    {
+        // Load lại scene hiện tại
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+    }
+}
